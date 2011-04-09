e00744c47111c64491a47fcffbe12e8f69c08d45
Joeyy <Joeyverlaan@gmail.com>
2011-04-09 16:19:52 +0200

    Fixed command

--- src/main/java/me/Joeyy/Armor/GiveArmor.java
+++ src/main/java/me/Joeyy/Armor/GiveArmor.java
@@ -14,7 +14,6 @@ public class GiveArmor implements CommandExecutor{
 		this.plugin = instance;
 	}
 
-	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
 		if (args.length < 2)
